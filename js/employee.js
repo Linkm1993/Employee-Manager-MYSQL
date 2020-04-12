d77a94b5e67e993b336878a02dc3f029b07f209d
Matt Link <Linkm1993@gmail.com>
2020-04-11 21:26:40 -0400

    Now able to add a new employees in database

--- js/employee.js
+++ js/employee.js
@@ -1,16 +1,57 @@
 const inquirer = require("inquirer")
+const mysql = require("mysql")
+
+const connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "",
+  database: "company_db"
+});
+
 
 function newEmployee(){
   inquirer
     .prompt([ {
-      type: 'list',
-      name: 'size',
-      message: 'Test',
-      choices: ['Jumbo'],
+      type: 'input',
+      name: 'first_name',
+      message: `Enter the employee's first name`
+    },
+    {
+      type: 'input',
+      name: 'last_name',
+      message: `Enter the employee's last name`
+    },
+    {
+      type: 'input',
+      name: 'manager_id',
+      message: `Enter the employee's manage id number`
     }
   ])
   .then(answers => {
     console.log(JSON.stringify(answers, null, '  '));
+
+    connection.connect(function(err) {
+      if (err) throw err;
+      console.log("connected as id " + connection.threadId + "\n");
+      var query = connection.query(
+        "INSERT INTO employee SET ?",
+        {
+          first_name: answers.first_name,
+          last_name: answers.last_name,
+          manager_id: answers.manager_id
+        },
+        function(err, res) {
+          if (err) throw err;
+        })
+        connection.end();
+      });
   });
 }
 
